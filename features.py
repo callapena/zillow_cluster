fb545ab6214e7696ba474f63548a88c4c4e3e142
Sam Callahan <callahan.l.sam@gmail.com>
2019-11-06 15:25:46 -0600

    Working prepare file and features notes

--- features.py
+++ features.py
@@ -3,3 +3,11 @@ import pandas as pd
 import numpy as np
 from acquire import get_zillow
 from prepare import prep_zillow
+
+# HOUSE AGE
+
+# YES/NO AIRCONDITIONING, FIREPLACE, HEATING
+
+# COMBINED BATHS & BEDS
+
+# TAX RATE
--- prepare.py
+++ prepare.py
@@ -5,7 +5,7 @@ import numpy as np
 
 # from bayes methodologies repo, fills zeros in nulls
 def fill_zero(df, cols):
-    df.fillna(value=0, inplace=True)
+    df.loc[:, cols] = df.loc[:, cols].fillna(value=0)
     return df
 
 # from bayes methodologies repo, fills zeros in nulls
@@ -30,19 +30,27 @@ def drop_bad_zeros(df, cols):
         df = df.drop(df[df[col] == 0].index)
     return df
 
-# FEATURES IDEAS:
-# tax rate, house age
+# OUTLIERS TO DROP
+# baths > 7, beds > 7, sqft > 10k, garages > 5, sqft > 10k, lot > 30000
 
-def prep_zillow(df):
+def prep_zillow(df, drop_outliers=True):
     prep = drop_id(df)
     prep = handle_missing_values(prep, .1, .6)
     # drop columns that appear to provide little information
-    prep.drop(columns=['assessmentyear', 'unitcnt', 'finishedsquarefeet12', 'propertylandusetypeid', 'rawcensustractandblock', ], inplace=True)
+    prep.drop(columns=['assessmentyear', 'unitcnt', 'finishedsquarefeet12', 'propertylandusetypeid', 'rawcensustractandblock', 'censustractandblock',
+                        'threequarterbathnbr'], inplace=True)
     prep = drop_bad_zeros(prep, ['bedroomcnt', 'bathroomcnt', 'calculatedfinishedsquarefeet'])
-    # to_zero = []
-    # prep = fill_zero(prep, to_zero)
-    # lazy = {}
-    # prep = prep.rename(lazy)
+    lazy = {'logerror': 'logerror', 'transactiondate': 'date', 'airconditioningtypeid': 'ac', 'bathroomcnt': 'baths', 'bedroomcnt': 'beds',
+            'buildingqualitytypeid': 'quality', 'calculatedbathnbr': 'calculatedbathnbr', 'calculatedfinishedsquarefeet': 'sqft',
+            'fips': 'fips', 'fireplacecnt': 'fireplace', 'fullbathcnt': 'fullbaths', 'garagecarcnt': 'garage', 'garagetotalsqft': 'garagesqft',
+            'heatingorsystemtypeid': 'heating', 'latitude': 'lat', 'longitude': 'long', 'lotsizesquarefeet': 'lotsqft', 'poolcnt': 'pool', 'pooltypeid7': 'pooltype',
+            'propertycountylandusecode': 'usecode', 'propertyzoningdesc': 'zoning', 'regionidcity': 'city', 'regionidcounty': 'altcounty',
+            'regionidneighborhood': 'neighborhood', 'regionidzip': 'zip', 'roomcnt': 'rooms', 'yearbuilt':'year',
+            'numberofstories': 'stories', 'structuretaxvaluedollarcnt': 'strucvalue', 'taxvaluedollarcnt': 'value', 'landtaxvaluedollarcnt': 'landvalue',
+            'taxamount': 'tax'}
+    prep = prep.rename(columns=lazy)
+    to_zero = ['fireplace', 'fullbaths']
+    prep = fill_zero(prep, to_zero)
     return prep
 
 if __name__ == '__main__':
