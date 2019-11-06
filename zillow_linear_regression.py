54840a9bab8309afe17c430411d4cb00dee7ecde
SaraPena <pena.e.sara@gmail.com>
2019-11-06 11:46:58 -0600

    Create zillow_linear_regression.py file
    Initial modeling with basic features.

---
+++ zillow_linear_regression.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
+from math import sqrt
+import warnings
+warnings.filterwarnings("ignore")
+
+import env
+import acquire
+import prepare
+
+df = acquire.get_zillow()
+df = df[['logerror', 'parcelid','calculatedfinishedsquarefeet','fips','latitude', 'longitude', 'lotsizesquarefeet', 'yearbuilt', 'taxvaluedollarcnt']]
+df.info()
+df.yearbuilt.value_counts().sort_index()
+df['home_age'] = 2017 - df.yearbuilt
+df.home_age.value_counts().sort_index()
+
+df.info()
+df = df.drop(columns = ['yearbuilt'])
+
+df.set_index('parcelid', inplace = True)
+
+sns.distplot(df.logerror)
+sns.distplot(df.calculatedfinishedsquarefeet)
+
+df.calculatedfinishedsquarefeet.value_counts(dropna=False)
+df.dropna(axis = 0)
+
+train, test = train_test_split(df, random_state = 123)
+
+train.info()
+train.calculatedfinishedsquarefeet.value_counts(dropna=False)
+train = train.fillna(0)
+train.info()
+
+train.corr()
+
+X = train[['calculatedfinishedsquarefeet', 'lotsizesquarefeet', 'taxvaluedollarcnt', 'home_age', 'fips']]
+y = train[['logerror']]
+
+from sklearn.linear_model import LinearRegression
+
+lm1 = LinearRegression()
+
+lm1.fit(X, y)
+
+y_pred_lm1 = lm1.predict(X)
+
+mean_squared_error(y, y_pred_lm1)
+
+r2_score(y, y_pred_lm1)
+
+y.index.values.reshape(-1,1)
+
+y_pred_baseline = np.array([y.mean()[0]]*len(y))
+
+
+model = pd.DataFrame({'actual': y.logerror,
+              'lm1': y_pred_lm1.ravel(),
+              'y_baseline': y_pred_baseline.ravel(),
+              'fips': X.fips})
+
+sns.relplot(x = 'lm1', y='actual', data=model)
+sns.relplot(x = 'lm1', y='y_baseline', data=model, color = 'red')
+plt.show()
