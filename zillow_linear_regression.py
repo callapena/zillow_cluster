cb69cee658df738f334f960fceac316afeecd73c
SaraPena <pena.e.sara@gmail.com>
2019-11-08 14:03:28 -0600

    Update zillow_linear_regression.py file.

--- zillow_linear_regression.py
+++ zillow_linear_regression.py
@@ -14,38 +14,42 @@ import acquire
 import prepare
 
 df = acquire.get_zillow()
-df = df[['logerror', 'parcelid','calculatedfinishedsquarefeet','fips','latitude', 'longitude', 'lotsizesquarefeet', 'yearbuilt', 'taxvaluedollarcnt']]
+df = prepare.prep_zillow(df)
+df = df[['logerror','sqft','fips','lat', 'long', 'lotsqft', 'age', 'tax']]
 df.info()
-df.yearbuilt.value_counts().sort_index()
+df.age.value_counts(bins = 10).sort_index()
+
 
 #### Home
-df['home_age'] = 2017 - df.yearbuilt
-df.home_age.value_counts().sort_index()
+# df['home_age'] = 2017 - df.yearbuilt
+# df.home_age.value_counts().sort_index()
 
-df.info()
-df = df.drop(columns = ['yearbuilt'])
+# df.info()
+# df = df.drop(columns = ['yearbuilt'])
 
-df.set_index('parcelid', inplace = True)
+# df.set_index('parcelid', inplace = True)
 
 sns.distplot(df.logerror)
-sns.distplot(df.calculatedfinishedsquarefeet)
+plt.xlim(-1,1)
+plt.ylim(0,8)
+sns.distplot(df.sqft)
 
-df.calculatedfinishedsquarefeet.value_counts(dropna=False)
-df.dropna(axis = 0)
+df.sqft.value_counts(dropna=False)
 
 train, test = train_test_split(df, random_state = 123)
 
 train.info()
-train.calculatedfinishedsquarefeet.value_counts(dropna=False)
+train.sqft.value_counts(dropna=False)
 train = train.fillna(0)
 train.info()
 
 train.corr()
 
-X = train[['calculatedfinishedsquarefeet', 'lotsizesquarefeet', 'taxvaluedollarcnt', 'home_age', 'fips']]
+X = train[['sqft', 'lotsqft', 'tax', 'age']]
 y = train[['logerror']]
 
 from sklearn.linear_model import LinearRegression
+X.info()
 
 lm1 = LinearRegression()
 
@@ -55,7 +59,9 @@ y_pred_lm1 = lm1.predict(X)
 
 
 ###Scores
-mean_squared_error(y, y_pred_lm1)
+mean_squared_error(y, y_pred_lm1)**1/2
+
+
 
 r2_score(y, y_pred_lm1)
 
@@ -72,3 +78,6 @@ model = pd.DataFrame({'actual': y.logerror,
 sns.relplot(x = 'lm1', y='actual', data=model)
 sns.relplot(x = 'lm1', y='y_baseline', data=model, color = 'red')
 plt.show()
+
+
+
